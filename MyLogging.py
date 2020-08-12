8ee526892c78a993057051b1389fc4b7a1a4075e
edire <eric.dire@puritanlife.com>
2020-08-12 08:42:08 -0700

    Add logging level logic to MyLogging for each logger

--- MyLogging.py
+++ MyLogging.py
@@ -8,12 +8,18 @@ import datetime as dt
 
 file_name = 'log_' + str(dt.datetime.today().strftime('%Y%m%d_%H%M%S')) + '.txt'
 directory = os.path.join(os.getcwd(), 'logs')
+log_level = 20
 
 
 def LogDirectory(directory_new):
     global directory
     directory = os.path.join(directory_new, 'logs')
-
+    
+    
+def LogLevel(level_new):
+    global log_level
+    log_level = level_new
+    
 
 def NewLogger(logger_name):
     if not os.path.exists(directory):
@@ -21,6 +27,7 @@ def NewLogger(logger_name):
     log_file =  os.path.join(directory, file_name)
     
     logger = logging.getLogger(logger_name)
+    logger.setLevel(log_level)
     file_handler = logging.FileHandler(log_file)
     formatter = logging.Formatter('%(asctime)s: %(name)s: %(levelname)s: %(message)s')
     file_handler.setFormatter(formatter)
