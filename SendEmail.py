90447b141e67d893ed6799e785c03de2c8d540c1
EDiRe <eric.dire@puritanlife.com>
2021-01-25 20:51:09 -0700

    Update SendEmail for SendGrid API

--- SendEmail.py
+++ SendEmail.py
@@ -3,13 +3,45 @@
 import smtplib
 from email.message import EmailMessage
 import os
-#import imghdr #for attaching pictures to email
-
-
-#body_html = textile.textile(string)
+import sendgrid
+import sendgrid.helpers.mail as sgm
+import base64
 
 
 def SendEmail(to_email_addresses, subject, body, body_html=None, attach_file_address=None, cc_email_addresses=None):
+
+    from_email = "Admin@puritanlife.com"
+    mail = sgm.Mail(from_email=from_email, to_emails=to_email_addresses, subject=subject, plain_text_content=body, html_content=body_html)
+    mail.reply_to = sgm.ReplyTo('eric.dire@puritanlife.com')
+
+    if attach_file_address != None:
+        with open(attach_file_address, 'rb') as f:
+            data = f.read()
+        encoded = base64.b64encode(data).decode()
+        attachment = sgm.Attachment()
+        attachment.file_content = sgm.FileContent(encoded)
+        attachment.file_type = sgm.FileType("application/pdf")
+        attachment.file_name = sgm.FileName(os.path.basename(attach_file_address))
+        attachment.disposition = sgm.Disposition("attachment")
+        # attachment.content_id = sgm.ContentId('Example Content ID')
+        mail.add_attachment(attachment)
+
+    if cc_email_addresses != None:
+        for email_address in cc_email_addresses:
+            mail.add_cc(email_address)
+
+    sg = sendgrid.SendGridAPIClient(api_key = os.environ.get('SendGridAPIKey'))
+    sg.send(mail)
+
+
+
+###############################################################################################################################################
+
+
+#import imghdr #for attaching pictures to email
+#body_html = textile.textile(string)
+
+def SendEmailCred(to_email_addresses, subject, body, body_html=None, attach_file_address=None, cc_email_addresses=None):
 
     email_user = os.environ.get('SendGridUID')
     email_password = os.environ.get('SendGridPWD')
@@ -41,6 +73,9 @@ def SendEmail(to_email_addresses, subject, body, body_html=None, attach_file_add
 
 
 
+###############################################################################################################################################
+
+
 # def SendEmailLocal(to_email_addresses, subject, body):
 #     email_user = 'SSIS@local.ed'
 #     email_password = 'SSIS2019Password'
@@ -54,9 +89,3 @@ def SendEmail(to_email_addresses, subject, body, body_html=None, attach_file_add
 #     with smtplib.SMTP('localhost', port=25) as smtp:
 #         smtp.login(email_user, email_password)
 #         smtp.send_message(msg)
-
-
-
-# if __name__ == '__main__':
-#     SendEmailLocal('eric.dire@puritanlife.com', 'subject', 'body')
-#     print('Sent')
